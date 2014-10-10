6a4b534703b41fb57f19f58db8a68019ee0cb70a
James Brown <james@insityou.uk>
2014-10-10 10:56:14 +0100

    Adds a useful function
    Adds a non-recursive implementation of 'getting the nth term of the
    fibonacci sequence'.

--- Ui/ManagementUi/mainwindow.cpp
+++ Ui/ManagementUi/mainwindow.cpp
@@ -26,6 +26,25 @@ int testingMergeViaGitHubAppWithAnInt()
     return 1000;
 }
 
+// The space complexity of this function is constant
+// and the time complexity is linear.
+int functionThatDoesSomethingInteresting(int n)
+{
+    if(0 == n || 1 == n)
+        return 1;
+
+    int a = 2, b = 3;
+
+    for(int i = 2; i != n; ++i)
+    {
+        int temp = a + b;
+        a = b;
+        b = temp;
+    }
+
+    return a;
+}
+
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
